3c0cb1c8f8c58d02cf60646fc2f2e04ef2b58d3c
Daniele Morosinotto <d.morosinotto@icloud.com>
2022-08-02 17:56:20 +0200

    experiment with datepicker JQuery

--- projects/web-app/src/app/app.component.ts
+++ projects/web-app/src/app/app.component.ts
@@ -7,12 +7,14 @@ import { SharedService } from "@app/shared";
         <h1>Welcome to {{ title }}!</h1>
         <router-outlet></router-outlet>
         <!-- <lib-shared></lib-shared> -->
-        <app-try-plugin></app-try-plugin>
+        <app-try-plugin [(valueUTC)]="birtday" [format]="'ITA dd/mm/yy'"></app-try-plugin>
+        <h2>birtday: {{ birtday }}</h2>
     `,
     styles: [],
 })
 export class AppComponent {
     title = "web-app";
+    birtday = "1975-03-20Z";
     constructor(private shared: SharedService) {
         shared.log(this.title);
     }
--- projects/web-app/src/app/try-plugin/try-plugin.component.ts
+++ projects/web-app/src/app/try-plugin/try-plugin.component.ts
@@ -1,46 +1,99 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, Output, ViewChild } from "@angular/core";
 import { CommonModule } from "@angular/common";
 declare var $: any;
+const UTC_FORMAT = "yy-mm-ddZ";
+type stringUTC = string;
 @Component({
     selector: "app-try-plugin",
     standalone: true,
     imports: [CommonModule],
     template: ` <p>Date: <input type="text" id="sampleDTPicker" /></p>
         <div class="flex">
-            <div>Date: <input type="text" #dtp /><button (click)="open()">DTP</button></div>
-            <span>{{ birthday }}<button id="show" (click)="dialog($event)">x</button></span>
+            <input type="text" #inp (input)="parse()" />
+            <span>Date: {{ valueUTC }}<button id="show" (click)="dialog($event)">x</button></span>
         </div>`,
-    styles: [".flex { display: flex }"],
+    styles: ["div { display: flex }"],
 })
 export class TryPluginComponent implements AfterViewInit {
-    constructor() {}
+    constructor() {
+        console.log("on ctor", this.inp?.nativeElement ?? "NOT READY!");
+    }
+
+    @Input() format = "dd/mm/yyyy";
+    @Input() set valueUTC(value: stringUTC) {
+        if (this._value != value) {
+            console.info("SET", value);
+            var date = $.datepicker.parseDate(UTC_FORMAT, value);
+            if (date != null) {
+                this._value = value;
+                this.valueUTCChange.emit(value);
+                setTimeout(() => (this.inp.nativeElement.value = $.datepicker.formatDate(this.format, date)));
+            }
+        } else {
+            console.info(value, "IS SAME");
+        }
+    }
+    get valueUTC(): stringUTC {
+        return this._value;
+    }
+    private _value!: stringUTC;
+    private _onChange = (v: string) => this.valueUTCChange.emit(v);
+    @Output() valueUTCChange = new EventEmitter<string>();
+
+    ngOnChange() {
+        console.log("on ngOnChange", this.inp?.nativeElement ?? "NOT READY!");
+    }
+
+    ngOnInit() {
+        console.log("on ngOnInit", this.inp?.nativeElement ?? "NOT READY!");
+    }
 
     ngAfterViewInit(): void {
+        console.log("on ngAfterViewInit", this.inp?.nativeElement ?? "NOT READY!");
         // $(function () {
-        $("#sampleDTPicker").datepicker({ showButtonPanel: true, showOn: "button", dateFormat: "dd/mm/yy" });
+        $("#sampleDTPicker").datepicker({ showButtonPanel: true, showOn: "button", dateFormat: this.format });
         var str = $.datepicker.formatDate("yy-mm-dd", new Date(1975, 3 - 1, 20)); //LOCALTIME
         var d = $.datepicker.parseDate("yy-mm-dd", str); //LOCALTIME
         console.log("format", str, "parse", d, "UTC", d.toISOString());
         // });
     }
 
-    @ViewChild("dtp", { read: ElementRef, static: true }) inp!: ElementRef;
-    open() {
-        console.log(this.inp);
-        $(this.inp.nativeElement).datepicker({ dateFormat: "dd/mm/yy" });
+    @ViewChild("inp", { read: ElementRef, static: true }) inp!: ElementRef;
+    parse() {
+        var dateText = this.inp.nativeElement.value;
+        console.log(this.inp, "value", dateText);
+        var date = $.datepicker.parseDate(this.format, dateText);
+        if (date != null) {
+            this.valueUTC = $.datepicker.formatDate(UTC_FORMAT, date);
+        }
+
+        //     $(this.inp.nativeElement)
+        //         .datepicker({
+        //             dateFormat: this.format,
+        //             showOn: "button",
+        //             disabled: false,
+        //             onSelect: (dateText: string, dp: any) => {
+        //
+        //                 console.log(dateText, "parse", parse);
+        //                 // this.birthday = parse;
+        //                 $(dp.input).datepicker("option", "disabled", !dateText);
+        //             },
+        //         })
+        //         .datepicker("option", "disabled", false);
     }
 
-    birthday = "1975-03-20";
     dialog(e: MouseEvent) {
         console.log("$event", e);
         $(e.target).datepicker(
             "dialog",
-            this.birthday,
+            this.valueUTC,
             (...args: any[]) => {
                 console.log("OnSelect", args);
-                this.birthday = args[0];
+                this.valueUTC = args[0]; //STRING FORMATTED
+                var date = $(args[1].input).datepicker("getDate");
+                console.log("model", date); //Date OBJECT LT
             },
-            { showButtonPanel: true, dateFormat: "yy-mm-dd" },
+            { showButtonPanel: true, dateFormat: UTC_FORMAT, altField: this.inp.nativeElement, altFormat: this.format },
             [e.clientX, e.clientY]
         );
     }
